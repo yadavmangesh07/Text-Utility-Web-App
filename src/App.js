c8db14daabf025623ef7d611d588e8da1f8beae1
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-02-11 19:02:14 +0530

    Added Alert For Mode Changing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import NavBar from './component/NavBar';
 import TextForm from './component/TextForm';
+import Alert from './component/Alert';
 import React, { useState } from 'react';
+
+
 
 function App() {
   const [mode, setMode] = useState('light');
   const [text, setText] = useState('dark');
   const [btnText, setBtnText] = useState('Dark');
   const [isBold, setIsBold] = useState(false);
+  const [alert, setAlert] = useState(null);
+const showAlert=(message,type)=>{
+  setAlert({
+    msg:message,
+    typ:type
+  })
+}
+  
 
   const changeMode = () => {
     if (mode === 'dark') {
@@ -16,18 +27,21 @@ function App() {
       setBtnText('Dark');
       document.body.style.backgroundColor = "white";
       document.body.style.color = "#212529";
+      showAlert('Light Mode Enabled','success');
     } else {
       setMode('dark');
       setText('light');
       setBtnText('Light');
       document.body.style.backgroundColor = "#212529";
       document.body.style.color = "white";
+      showAlert('Dark Mode Enabled','success');
     }
   };
 
   return (
     <>
       <NavBar title="Textify" about="About" mode1={mode} textColor={text} buttonText={btnText} toggleMode={changeMode} />
+      <Alert alert={alert}/>
       <div className="container">
         <TextForm heading="Enter The Text To Be Analyzed" isBold={isBold} setIsBold={setIsBold} mode1={mode}/>
       </div>
---
+++ src/component/Alert.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Alert(props) {
+    const capitalize=(word)=>{
+        const str=word.toLowerCase();
+        return str.charAt(0).toUpperCase()+str.slice(1)+':';
+
+    }
+    return (
+        props.alert &&
+            <div className={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
+  <strong>{capitalize(props.alert.typ)}</strong> {props.alert.msg}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+        
+    )
+}
