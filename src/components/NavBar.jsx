f99a4fc5cbbe98873c1933c22d7e63e8d8bee6dc
Abhishek Nath Tiwari <110283974+abhint1@users.noreply.github.com>
2024-02-24 22:56:30 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,17 +9,17 @@ export default function NavBar(props) {
   
       <nav className="navbar navbar-expand-lg bg-body-primary ">
         <div className="container-fluid " >
-          <a className={`navbar-brand text-${props.textColor}`} href="/">{props.title}</a>
+          <a className={navbar-brand text-${props.textColor}} href="/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item ">
-                <a className={`nav-link active text-${props.textColor}`} aria-current="page" href="/">Home</a>
+                <a className={nav-link active text-${props.textColor}} aria-current="page" href="/about">Home</a>
               </li>
               <li className="nav-item">
-                <a className={`nav-link active text-${props.textColor}`} href="/">{props.about}</a>
+                <a className={nav-link active text-${props.textColor}} href="/about">{props.about}</a>
               </li>
 
             </ul>
@@ -31,7 +31,7 @@ export default function NavBar(props) {
       </nav>
             <div className="form-check form-switch mx-3" >
               <input className="form-check-input " onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-              <label className={`form-check-label text-${props.textColor}`} htmlFor="flexSwitchCheckDefault">Enable {props.buttonText} Mode</label>
+              <label className={form-check-label text-${props.textColor}} htmlFor="flexSwitchCheckDefault">Enable {props.buttonText} Mode</label>
             </div>
 </nav>
     </>
@@ -45,4 +45,3 @@ export default function NavBar(props) {
 //   React: 'bhfbewfb',
 
 // }
-
