110dc89f26c4edebf71bee26df4dd836554beff6
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-03-01 10:29:33 +0530

    GitHub Pages Related Changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-    plugins:[react()],
-    server:{
-        port:3000,
-        open:true
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    rollupOptions: {
+      external: ['web-vitals']
     }
-})
+  },
+  resolve: {
+    alias: {
+      '@': '/Users/mangeshyadav/my-app/src' // Adjust the path as per your project structure
+    }
+  }
+});
